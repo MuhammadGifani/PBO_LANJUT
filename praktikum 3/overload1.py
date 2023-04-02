8cb5feb847a4d71906777567148f32df6d4beb79
MuhammadGifani <126759151+MuhammadGifani@users.noreply.github.com>
2023-04-02 11:37:06 +0700

    Add files via upload

---
+++ praktikum 3/overload1.py
@@ -0,0 +1,6 @@
+#Nama : Muhammad Gifani
+#NIM : 210511051
+#Kelas : R2
+print(len("Muhammad Gifani")) 
+print(len((1,2,3,4,5,6,7,10)))
+print(len(("210511051")))
---
+++ praktikum 3/overload2.py
@@ -0,0 +1,11 @@
+#Nama : Muhammad Gifani 
+#NIM : 210511051
+#Kelas : R2
+
+print(2 + 5)
+print("Muhammad" + "Gifani")
+print([2, 1] + [0, 5] + [1, 1] + [0, 5, 1])
+
+print(max(1,2,5,10))
+print(max([1, 2, 10]))
+print(max("Gifani"))
