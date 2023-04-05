794e793bc6a852505434f71bc622cabb1c983c90
MuhammadGifani <126759151+MuhammadGifani@users.noreply.github.com>
2023-04-05 23:58:03 +0700

    Add files via upload

---
+++ Latihan3/contoh5.py
@@ -0,0 +1,14 @@
+#Implementasi Polymorphism Dinamis dalam Duck Typing
+
+class Kucing:
+    def bersuara(self):
+        print("Meow")
+class Anjing:
+    def bersuara(self):
+        print("Guk guk")
+    def cetak_suara(objek):
+        objek.bersuara()
+kucing = Kucing()
+anjing = Anjing()
+cetak_suara(kucing) # Output: Meow
+cetak_suara(anjing) # Output: Guk guk
